1346644c9e0ea1c89168f0397cd083c699fb4662
jaivanthm24-cyber <jaivanthm24@gmail.com>
2026-07-20 14:54:26 +0530

    Time: 3 ms (6.70%) | Memory: 46.1 MB (5.08%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -8,27 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) return null;
-        
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        ListNode curr = head;
+        while (curr != null) {
+            if (seen.contains(curr.val)) {
+                duplicates.add(curr.val);
+            }
+            seen.add(curr.val);
+            curr = curr.next;
+        }
         ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode current = head;
-        
-        while (current != null) {
-            while (current.next != null && current.val == current.next.val) {
-                current = current.next;
+        ListNode tail = dummy;
+        curr = head;
+        while (curr != null) {
+            if (!duplicates.contains(curr.val)) {
+                tail.next = new ListNode(curr.val);
+                tail = tail.next;
             }
-            
-            if (prev.next == current) {
-                prev = prev.next;
-            } else {
-                prev.next = current.next;
-            }
-            
-            current = current.next;
+            curr = curr.next;
         }
         
         return dummy.next;
