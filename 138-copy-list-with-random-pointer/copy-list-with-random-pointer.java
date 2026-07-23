68c2b06c28df631bda4d33e076b2c6a462e08081
jaivanthm24-cyber <jaivanthm24@gmail.com>
2026-07-23 11:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (94.80%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -12,50 +12,30 @@ class Node {
     }
 }
 */
-
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) {
-            return null;
-        }
+        if (head == null) return null;
 
-        Node cur = head;
-
-        // Step 1: Insert copied nodes after each original node
-        while (cur != null) {
+        for (Node cur = head; cur != null; cur = cur.next.next) {
             Node copy = new Node(cur.val);
             copy.next = cur.next;
             cur.next = copy;
-            cur = copy.next;
         }
 
-        // Step 2: Assign random pointers
-        cur = head;
-
-        while (cur != null) {
-            if (cur.random != null) {
+        for (Node cur = head; cur != null; cur = cur.next.next) {
+            if (cur.random != null)
                 cur.next.random = cur.random.next;
-            }
-
-            cur = cur.next.next;
         }
 
-        // Step 3: Separate the copied list
-        cur = head;
-        Node copyHead = head.next;
+        Node newHead = head.next;
 
-        while (cur != null) {
+        for (Node cur = head; cur != null; cur = cur.next) {
             Node copy = cur.next;
-
             cur.next = copy.next;
-
-            if (copy.next != null) {
+            if (copy.next != null)
                 copy.next = copy.next.next;
-            }
-
-            cur = cur.next;
         }
 
-        return copyHead;
+        return newHead;
     }
 }
