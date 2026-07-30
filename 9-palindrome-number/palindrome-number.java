aaf15ca4f57458b3acbf546d894e5a4d6811bf70
jaivanthm24-cyber <jaivanthm24@gmail.com>
2026-07-30 14:21:19 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (33.75%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n = x;
+        int rev = 0;
+
+        if (x < 0)
+            return false;
+
+        while (n != 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+
+        return x == rev;
+    }
+}
