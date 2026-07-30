07a7d47d5c5d0e7e394f2182c9df617bdc4084d9
jaivanthm24-cyber <jaivanthm24@gmail.com>
2026-07-30 11:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (19.12%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for (int i=0;i<accounts.length;i++){
+            int count=0;
+            for(int j=0;j<accounts[0].length;j++){
+                count+=accounts[i][j];
+            }
+            if(count>=max){
+                max=count;
+                }
+                count=0;
+        }return max;        
+    }
+}
