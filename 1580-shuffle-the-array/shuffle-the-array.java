b409de784b4f790cba8b1dec1177e2030d1f640f
JAIVANTH <jaivanthm24@gmail.com>
2026-07-31 09:37:50 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (96.16%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans=new int[n*2];int j=0;
+        for(int i=0;i<n;i++){
+            ans[j]=nums[i];
+            j++;
+            ans[j]=nums[i+n];
+            j++;
+        }return ans;        
+    }
+}
